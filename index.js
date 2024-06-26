3f5180ae569fce7bfad12e11c993201f2bc22a21
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-06-27 00:04:28 +0300

    Worked on Arrow functions

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = function () {
+  let a = 2000;
+  let b = 100;
+  return a / b;
+};
+const square = (parameter1) => {
+  let x = 2;
+  return parameter1 ** x;
+};
+const add = (num1, num2) => {
+  let a = 3;
+  let b = 4;
+  return a + b;
+};
+
+square(2);
